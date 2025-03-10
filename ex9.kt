dcf6bf7a1321f4222b541e8083e0b0d157d3d68a
Gabriel Santos <99769924+exquii@users.noreply.github.com>
2025-03-10 08:28:52 -0300

    ex9.kt

--- ex9.kt
+++ ex9.kt
@@ -2,13 +2,13 @@ fun main() {
     triangulo()
 }
 fun triangulo () {
-    println("Digite um segmento de uma reta: ")
+    println("Digite um segmento: ")
     val segmento1 = readln().toInt()
 
-    println("Digite um segmento de uma reta: ")
+    println("Digite um segmento: ")
     val segmento2 = readln().toInt()
 
-    println("Digite um segmento de uma reta: ")
+    println("Digite um segmento: ")
     val segmento3 = readln().toInt()
 
     if (segmento1 < (segmento2 + segmento3) && segmento2 < segmento1 + segmento3 && segmento3 < (segmento1 + segmento2)) {
